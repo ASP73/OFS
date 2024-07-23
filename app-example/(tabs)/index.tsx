61c47cc6f32002c6b998cf7f8e0f431c295c8f35
ASP73 <alecsparker@outlook.com>
2024-07-23 15:19:54 +0100

    Initial commit

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -11,9 +11,8 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
+          source={require('@/assets/images/AP_clay-removebg-preview.png')}
+          style={styles.reactLogo} />
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Hello World!</ThemedText>
